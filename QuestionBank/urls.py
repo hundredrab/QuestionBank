3253240c03c24d44fd680dea4cf7b3cc69275267
hundredrab <sourab.jha@kgec.edu.in>
2019-11-05 20:32:34 +0530

    Add drf docs.

--- QuestionBank/urls.py
+++ QuestionBank/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('question/', include('questions.urls')),
+    path('docs/', include('rest_framework_docs.urls')),
 ]
