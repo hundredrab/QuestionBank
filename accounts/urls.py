6eb678e947f707985f52df6739c84f929758c355
hundredrab <sourab.jha@kgec.edu.in>
2019-11-24 05:01:03 +0530

    Append / at the end of account urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', authviews.LoginView.as_view(), name='login'),
-    path('logout', authviews.LogoutView.as_view(), name='logout'),
+    path('login/', authviews.LoginView.as_view(), name='login'),
+    path('logout/', authviews.LogoutView.as_view(), name='logout'),
 ]
